aec416e3167929b5cf0592a3cfee913c493f1ca5
systemthinker <robbertoutenaar@gmail.com>
2021-10-17 20:07:05 +0200

    refactoring, mediabreakpoints

--- src/js/_domManipulation.js
+++ src/js/_domManipulation.js
@@ -1,38 +1,23 @@
 (function () {
   const newIcon =
     '<a class="title-icon" href=""><i class="fas fa-info-circle"></i></a>';
+  const classItemDescription = "article-item-desc";
 
-  const targetIcon = document.querySelectorAll('.article-item-productinfo');
-  const testIcon = document.querySelector('.article-item-img');
-  const title = document.querySelectorAll('.article-item-title');
-  console.log('target i', targetIcon[0].outerHTML);
-  console.log('testicon', testIcon);
-
-  const description = document.querySelectorAll('.article-item-desc');
+  const targetIcon = document.querySelectorAll(".article-item-productinfo");
+  const title = document.querySelectorAll(".article-item-title");
+  const description = document.querySelectorAll(`.${classItemDescription}`);
 
   title.forEach(
     (el, index) =>
       (el.innerHTML +=
-        ' ' +
+        " " +
         newIcon +
-        '<div className = "article-item-desc">' +
+        `<div class='${classItemDescription}'>` +
         description[index].innerHTML +
-        '</div>')
-  );
-  console.log(
-    'what is title',
-    title.forEach((el) => console.log(el))
+        "</div>")
   );
 
-  // remove previous icons
+  // remove previous icons and descriptions
   targetIcon.forEach((el) => el.remove());
+  description.forEach((desc) => desc.remove());
 })();
-// const appendStyles = (href) => {
-//   const head = document.head;
-//   const link = document.createElement("link");
-//   link.type = "text/css";
-//   link.rel = "stylesheet";
-//   link.href = href;
-
-//   head.appendChild(link);
-// };
