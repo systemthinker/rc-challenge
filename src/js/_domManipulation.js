c0cacd67d2855cd1253240dedbbaf5f22d579928
systemthinker <robbertoutenaar@gmail.com>
2021-10-17 16:50:25 +0200

    install saved

--- src/js/_domManipulation.js
+++ src/js/_domManipulation.js
@@ -1,14 +1,31 @@
 (function () {
-  const icon = '<a class="title-icon" href=""><i class="fas fa-info-circle"></i></a>';
-  const targetIcon = document.querySelectorAll('.article-item-productinfo');
-  const title = document.querySelectorAll('.article-item-title');
-  targetIcon.forEach((el) => el.remove());
+  const newIcon =
+    '<a class="title-icon" href=""><i class="fas fa-info-circle"></i></a>';
 
-  title.forEach((el, index) => (el.innerHTML += icon));
+  const targetIcon = document.querySelectorAll('.article-item-productinfo');
+  const testIcon = document.querySelector('.article-item-img');
+  const title = document.querySelectorAll('.article-item-title');
+  console.log('target i', targetIcon[0].outerHTML);
+  console.log('testicon', testIcon);
+
+  const description = document.querySelectorAll('.article-item-desc');
+
+  title.forEach(
+    (el, index) =>
+      (el.innerHTML +=
+        ' ' +
+        newIcon +
+        '<div className = "article-item-desc">' +
+        description[index].innerHTML +
+        '</div>')
+  );
   console.log(
     'what is title',
     title.forEach((el) => console.log(el))
   );
+
+  // remove previous icons
+  targetIcon.forEach((el) => el.remove());
 })();
 // const appendStyles = (href) => {
 //   const head = document.head;
